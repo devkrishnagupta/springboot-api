a804ca44609eccad65157d0b1c2300e402a6d6c7
D K Gupta <033dkg@gmail.com>
2022-06-03 07:41:42 +0530

    added logger

--- src/main/java/net/javaguides/springboot/controller/DepartmentController.java
+++ src/main/java/net/javaguides/springboot/controller/DepartmentController.java
@@ -2,9 +2,11 @@ package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.model.Department;
 import net.javaguides.springboot.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -14,18 +16,21 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
     @PostMapping
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside the get departments");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("{id}")
     public Department fetchDepartmentById( @PathVariable("id") Long departmentId){
+        LOGGER.info("Inside the get departments by id");
         return departmentService.fetchDepartmentById(departmentId);
     }
 
--- src/main/java/net/javaguides/springboot/model/Department.java
+++ src/main/java/net/javaguides/springboot/model/Department.java
@@ -1,15 +1,22 @@
 package net.javaguides.springboot.model;
 
+//import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.Size;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long departmentId;
+    @NotBlank(message = "Please add Department Name")
+//    @Length(max = 5, min = 1)
+//    @Size(max = 10, min = 0)
     private String departmentName;
     private String departmentAddress;
     private String getDepartmentCode;
